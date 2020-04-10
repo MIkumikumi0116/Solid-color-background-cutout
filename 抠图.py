b799ef069391905ebabb548ccfde0eaa8e9a1d7b
MIkumikumi0116 <57623431+MIkumikumi0116@users.noreply.github.com>
2020-04-10 08:35:57 +0800

    Update 抠图.py

--- 抠图.py
+++ 抠图.py
@@ -1,29 +1,35 @@
 from os import walk
 from PIL import Image
+import numpy as np
 
-#判断这个像素是不是要抠掉，要被抠掉就返回ture
-def cuttedOrNot(pixdate,color):
-    if abs(pixdate[0]-color[0]) <= tolerance and abs(pixdate[1]-color[1]) <= tolerance and abs(pixdate[2]-color[2]) <= tolerance:
-        return True
-    else:
-        return False
-
-#抠图
+#抠图方法，以最左上角为背景色，把背景抠掉
 def cut(root,filename):
-    img = Image.open(root+'\\'+filename)
+    img = Image.open(root + '\\' + filename)
     img = img.convert("RGBA")
- 
-    pixdata = img.load()
-    #指定要抠掉的颜色为左上角第一个像素颜色
-    color = pixdata [0,0]
+    #把图片转化成数组，加快速度
+    pixdata = np.array(img)
 
-    #先把外面一圈的白色像素扣掉，上下左右指要留下区域的上下左右边界
-    left,upper,right,lower = 0,0,0,0
-    #四次遍历图片，确定上下左右边界
+    #透明的RGBA通道数值
+    transparent = np.array([0,0,0,0])
+
+    
+    #当最左上角的像素是透明，是已经抠过一次，有死角没扣到，手动修正一下又来抠，就要手动指定颜色
+    if all(pixdata[0,0] == transparent):
+        r,g,b = list(map(eval,input("以RGB值指定背景颜色，用空格间隔\n").split()))
+        color = np.array([r,g,b,255])
+    else:
+    #否则以最左上角为背景色，把背景抠掉
+        color = pixdata[0,0]
+    
+
+    #先把外面一圈的纯色像素扣掉，既是为了抠出图片的主体，也是为了减小复杂度，上下左右指要留下区域的上下左右边界
+    left,upper,right,lower = 10,0,0,0
+    #从上下左右四个方向四次遍历图片，确定上下左右边界
     flag = 0
     for x in range(img.size[0]):
         for y in range(img.size[1]):
-            if not cuttedOrNot(pixdata[x,y],color):
+                #如果某个像素和背景色的差值大于容差，就要留下不抠，第一个这样的像素就是边界的位置
+            if  any(abs(pixdata[y,x] - color) > tolerance):
                 left = x
                 flag = 1
                 break
@@ -33,7 +39,7 @@ def cut(root,filename):
     flag = 0
     for y in range(img.size[1]):
         for x in range(img.size[0]):
-            if not cuttedOrNot(pixdata[x,y],color):
+            if any(abs(pixdata[y,x] - color) > tolerance):
                 upper = y
                 flag = 1
                 break
@@ -41,9 +47,9 @@ def cut(root,filename):
             break
 
     flag = 0
-    for x in range(img.size[0]-1,-1,-1):
+    for x in range(img.size[0] - 1,-1,-1):
         for y in range(img.size[1]):
-            if not cuttedOrNot(pixdata[x,y],color):
+            if any(abs(pixdata[y,x] - color) > tolerance):
                 right = x
                 flag = 1
                 break
@@ -51,94 +57,121 @@ def cut(root,filename):
             break
 
     flag = 0
-    for y in range(img.size[1]-1,-1,-1):
+    for y in range(img.size[1] - 1,-1,-1):
         for x in range(img.size[0]):
-            if not cuttedOrNot(pixdata[x,y],color):
+            if any(abs(pixdata[y,x] - color) > tolerance):
                 lower = y
                 flag = 1
                 break
         if flag == 1 :
             break
+
     #边界确定了，抠出来，再刷新一下pixdata
-    img = img.crop((left,upper,right,lower))
-    pixdata = img.load()
+    img = img.crop((left,upper,right + 1,lower + 1))
+    pixdata = np.array(img)
+    print("第",i,"张图片，当前阶段1/3")
 
-    #再把白色的像素抠成透明的，route是一个二维列表，分别对应每一个像素，
-    #route[x][y] = 1说明这个像素是白色，而且有一条到达图片边界的路径，而且这条路径上全是白色像素
-    #就是说这个像素是白的，而且不是不是图片边缘内部的像素，像是眼睛的眼白这种，这样的像素就要抠成透明的
 
-    #建立route，指定初值全是0
-    route = list()
+    #预处理，先从左到右，从右到左，从上到下，从下到上各遍历一遍，遇到需要抠掉的像素就抠，遇到要留下的像素就跳到下一行或下一列，
+    #这样子一般能把大部分要抠掉的像素都抠掉，没抠到的才交给广度优先遍历，减小复杂度
+    #有没有预处理整个抠图过程中访问的像素数量是一样的，但预处理中访问每个像素的时间比广度优先更短，
+    #加入预处理相当于把一部分像素交给速度快的预处理，只有少量死角才由速度慢当不留死角的广度优先处理
+    #我的例子测试下来有预处理比没有快了1/6到1/7
     for x in range(img.size[0]):
-        route.append([0])
         for y in range(img.size[1]):
-            route[x].append([0])
-    
-    #再确定哪些像素要抠掉，原理是白色的，最边上的像素肯定要扣掉，就把那个像素的route改成1，
-    #然后不是最边上的，白色的，而且它上下左右至少有一个像素要扣掉，那这个像素也得抠掉
-    #这样子抠图就是由图片最外面一圈向内一圈圈地确定哪像像素要扣掉，图片内部的白色色块的边界因为周围至少有一个像素不是白色，就不会被扣掉，
-    #白色色块的内部因为边界的像素都不会被抠掉，而扫描像素又是一圈一圈向内扫描，所以也不会被扣掉
-    #但是一圈一圈地扫描像素代码写起来很麻烦，就改成从左到右，同时从上到下扫描一遍，再从上到下，从左到右扫描一遍，再从右到左、从上到下一遍，从下到上、从左到右一遍
-    #上面那样再扫描一遍，和从外圈到内圈效果一样
-    #然后为了适应边缘检测抠图后抠不干净的问题（见最下面的注释），指定如果手动抠掉白色色块里的几个像素,那它们周围的像素也得抠，这样子就得再扫描一遍每个像素
+                #如果某个像素四个通道数值和背景差值小于容差，就要抠掉
+            if all(abs(pixdata[y,x] - color) < tolerance):
+                pixdata[y,x] = transparent
+                #其他情况就只有遇到了应该留下的像素，跳到下一行或下一列
+            else:
+                break
 
-    for i in range(3):
+    for x in range(img.size[0]):
+        for y in range(img.size[1] - 1,-1,-1):
+            if all(abs(pixdata[y,x] - color) < tolerance):
+                pixdata[y,x] = transparent
+            else:
+                break
+
+    for y in range(img.size[1]):
         for x in range(img.size[0]):
-            for y in range(img.size[1]):
-                #由于列表越界的问题，边上一圈像素和中间的像素分开处理
-                if (x==0 or y==0 or x==img.size[0]-1 or y==img.size[1]-1) and cuttedOrNot(pixdata[x,y],color):
-                    route[x][y] = 1
-                elif ((not x==0) and (not y==0) and (not x==img.size[0]-1) and (not y==img.size[1]-1)) and cuttedOrNot(pixdata[x,y],color) and (route[x-1][y] == 1 or route[x][y-1] == 1 or route[x+1][y] == 1 or route[x][y+1] == 1):
-                    route[x][y] = 1
-                elif pixdata[x,y] == (0,0,0,0):
-                     route[x][y] = 1
+            if all(abs(pixdata[y,x] - color) < tolerance):
+                pixdata[y,x] = transparent
+            else:
+                break
 
-        for y in range(img.size[1]):
-            for x in range(img.size[0]):
-                if (x==0 or y==0 or x==img.size[0]-1 or y==img.size[1]-1) and cuttedOrNot(pixdata[x,y],color):
-                    route[x][y] = 1
-                elif ((not x==0) and (not y==0) and (not x==img.size[0]-1) and (not y==img.size[1]-1)) and cuttedOrNot(pixdata[x,y],color) and (route[x-1][y] == 1 or route[x][y-1] == 1 or route[x+1][y] == 1 or route[x][y+1] == 1):
-                    route[x][y] = 1
-                elif pixdata[x,y] == (0,0,0,0):
-                     route[x][y] = 1
+    for y in range(img.size[1]):
+        for x in range(img.size[0] - 1,-1,-1):
+            if all(abs(pixdata[y,x] - color) < tolerance):
+                pixdata[y,x] = transparent
+            else:
+                break
+    print("第",i,"张图片，当前阶段2/3")
 
-        for x in range(img.size[0]-1,-1,-1):
-            for y in range(img.size[1]):
-                if (x==0 or y==0 or x==img.size[0]-1 or y==img.size[1]-1) and cuttedOrNot(pixdata[x,y],color):
-                    route[x][y] = 1
-                elif ((not x==0) and (not y==0) and (not x==img.size[0]-1) and (not y==img.size[1]-1)) and cuttedOrNot(pixdata[x,y],color) and (route[x-1][y] == 1 or route[x][y-1] == 1 or route[x+1][y] == 1 or route[x][y+1] == 1):
-                    route[x][y] = 1
-                elif pixdata[x,y] == (0,0,0,0):
-                     route[x][y] = 1
 
-        for y in range(img.size[1]-1,-1,-1):
-            for x in range(img.size[0]):
-                if (x==0 or y==0 or x==img.size[0]-1 or y==img.size[1]-1) and cuttedOrNot(pixdata[x,y],color):
-                    route[x][y] = 1
-                elif ((not x==0) and (not y==0) and (not x==img.size[0]-1) and (not y==img.size[1]-1)) and cuttedOrNot(pixdata[x,y],color) and (route[x-1][y] == 1 or route[x][y-1] == 1 or route[x+1][y] == 1 or route[x][y+1] == 1):
-                    route[x][y] = 1
-                elif pixdata[x,y] == (0,0,0,0):
-                     route[x][y] = 1
+    #广度优先遍历的起点
+    starting_point = list()
+    #扫描所有像素
+    for y in range(1,img.size[1]-1):
+        for x in range(1,img.size[0]-1):
+               #这个像素不透明                      这个像素和背景差值小于容差                    这个像素的上下左右至少有一个是透明的（已经被抠掉了）
+            if all(pixdata[y,x] != transparent) and all(abs(pixdata[y,x] - color) < tolerance) and (pixdata[y,x - 1][3] == 0 or pixdata[y,x + 1][3] == 0 or pixdata[y - 1,x][3] == 0 or pixdata[y + 1,x][3] == 0):
+                #那这个像素是死角的边界
+                starting_point.append((x,y))
 
-    #哪些像素要抠确定好了，抠
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
-          if route[x][y] == 1:
-                pixdata[x, y] = (0, 0, 0, 0)
 
-    img.save( 'done\\' + filename)
+    #遍历用的栈
+    stack = list()
+    #剩余的遍历入口数量
+    length = len(starting_point)
+    for point in starting_point:
+        stack.append(point)
+        length -= 1
+        #广度优先的细节懒得写了，反正来看代码的肯定知道广度优先是啥
+        while True:
+            if len(stack) == 0:
+                break
+
+            x,y = stack.pop()
+
+            print("第",i,"张图片，当前阶段3/3,剩余待处理的区块：",length, "当前区块剩余像素数量：",len(stack))
+
+            #把当前访问的像素抠掉
+            pixdata[y,x] = transparent
+            
+
+            #如果上下左右的像素需要被抠掉就压栈            
+            if all(abs(pixdata[y,x + 1] - color) < tolerance) and all(pixdata[y,x + 1] != transparent):
+                stack.append((x + 1,y))
+                
+            if all(abs(pixdata[y,x - 1] - color) < tolerance) and all(pixdata[y,x - 1] != transparent):
+                stack.append((x - 1,y))
+                
+            if all(abs(pixdata[y - 1,x] - color) < tolerance) and all(pixdata[y - 1,x] != transparent):
+                stack.append((x,y - 1))
+                
+            if all(abs(pixdata[y + 1,x] - color) < tolerance) and all(pixdata[y + 1,x] != transparent):
+                stack.append((x,y + 1))
+                
+
+    img = Image.fromarray(np.uint8(pixdata))
+    img.save('done\\' + filename)
     
-
 
 
 
 #抠图容差，有些图背景可能是254，253之类的，当某个像素三个通道颜色都和背景之差都小于容差时会被抠掉
-tolerance = 5
-
+tolerance = eval(input("指定抠图颜色容差\n"))
+#正在处理第i张图片
+i = 0
 for root,dir,file in walk("source"):
     for filename in file:
+        i += 1
         #抠source文件夹里的所有图片，抠完了放在done文件夹里
         cut(root,filename)
+        print("第",i,"张图片抠图完成")
+
+print("都抠完了")
 
 #最后就是这个算法实现了边缘检测，但只能检测到算法认为的边缘，比如一条丝带围了一圈，在人看来中间一圈白的是要抠掉的，但是算法认不出来
 #这种可以手动用PS什么的点一个透明像素在里面，再跑一遍算法
